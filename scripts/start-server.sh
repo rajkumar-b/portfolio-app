0d6beab1dde37fea3ecb9111dd2e71183318a611
Rajkumar Baskar <rajkumarneville@gmail.com>
2023-08-25 21:43:06 -0400

    change port to 80

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -9,5 +9,5 @@ fi
 # Change to the project directory
 cd /var/www/html/portfolio-app
 
-# Start your Three.js server (replace with your actual command)
-npm start
+# Start the Three.js server 
+npm run dev -- --port 80
