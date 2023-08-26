3327974a31bf2f37c74539c1fb51c0f67f622f42
Rajkumar <54833237+rajkumar-b@users.noreply.github.com>
2023-08-26 01:20:02 -0400

    remove superuser priv (#12)

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -5,13 +5,13 @@
 cd /var/www/html/portfolio-app
 
 # Install project dependencies using npm
-sudo npm install
+npm install
 
 # Install three js in case not installed
-sudo npm install three
+npm install three
 
 # Install Vite
-sudo npm install -g vite@latest
+npm install -g vite@latest
 
 # Install node built-in server
-sudo npm install -g http-server
+npm install -g http-server
