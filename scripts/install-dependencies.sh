2cf23b2682bd9e49dd65f52569945b6390eb31e5
Rajkumar Baskar <rajkumarneville@gmail.com>
2023-08-25 21:01:06 -0400

    modify appsec

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,9 +6,3 @@ sudo yum update -y
 
 # Install Node.js and npm
 sudo yum install -y nodejs npm
-
-# Change to the project directory
-cd /var/www/html
-
-# Install project dependencies using npm
-npm install
