6400dcb7123ed06ae8b0a70bb7819e0a60ca56f6
Rajkumar Baskar <rajkumarneville@gmail.com>
2023-08-27 14:45:48 -0400

    Add lights and helpers to locate them

--- main.js
+++ main.js
@@ -17,15 +17,28 @@ camera.position.setZ(30);
 
 // Create three main components to add a figure: geometry, material and mesh
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xFF6347,
-  wireframe: true
-});
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
 const torus = new THREE.Mesh(geometry, material);
 
-// Add this figure to the scene and render
+// Add this figure to the scene
 scene.add(torus);
 
+// Add a light to the scene
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(5, 5, 5);
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add(pointLight, ambientLight);
+
+// Add a lighthelper to view pointlight position
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper);
+
+// Add a gridhelper to view overall positions
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+
+// Render the scene
 function animate(){
   requestAnimationFrame(animate);
   torus.rotation.x += 0.01;
@@ -34,4 +47,4 @@ function animate(){
   renderer.render(scene, camera);
 }
 
-animate()
+animate();
