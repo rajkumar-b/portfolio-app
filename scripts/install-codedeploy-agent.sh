7b5d8ae2a57855c7e484459c37d20f4bbbd93a8e
Rajkumar Baskar <rajkumarneville@gmail.com>
2023-12-14 09:20:34 -0500

    add SSL generation process for google domain

--- scripts/install-codedeploy-agent.sh
+++ scripts/install-codedeploy-agent.sh
@@ -6,3 +6,8 @@ yum install -y ruby
 wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
+
+# Install Certbot to generate SSL
+sudo yum install python3
+sudo python3 -m ensurepip --default-pip
+sudo pip install certbot
