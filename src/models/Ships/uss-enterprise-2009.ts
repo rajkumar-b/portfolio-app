c564b3a2010f8cbd82795c204ae980c517beff81
Rajkumar Baskar <rajkumarneville@gmail.com>
2023-12-21 14:27:27 -0500

    Switch from js to ts for modular coding

---
+++ src/models/Ships/uss-enterprise-2009.ts
@@ -0,0 +1,55 @@
+import * as THREE from 'three';
+import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
+import { iShip } from './iShip'; 
+class USSEnterprise2009 implements iShip {
+  uss_dae: string;
+  loader: ColladaLoader;
+  model: any;
+
+  constructor() {
+    this.uss_dae = '../../res/models/uss-enterprise-2009/model.dae';
+    this.loader = new ColladaLoader();
+    this.model = null;
+  }
+
+  loadModel(scene: THREE.Scene): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.loader.load(this.uss_dae, (collada: { scene: any; }) => {
+        this.model = collada.scene;
+        this.model.traverse((child: any) => {
+          if (child.type === 'LineSegments') {
+            child.visible = false;
+          }
+        });
+        scene.add(this.model);
+        resolve(this.model);
+      }, undefined, reject);
+    });
+  }
+
+  moveForward() {
+    if (this.model) {
+      this.model.position.z -= 10;
+    }
+  }
+
+  moveBackward() {
+    if (this.model) {
+      this.model.position.z += 10;
+    }
+  }
+
+  moveLeft() {
+    if (this.model) {
+      this.model.position.x -= 10;
+    }
+  }
+
+  moveRight() {
+    if (this.model) {
+      this.model.position.x += 10;
+    }
+  }
+}
+
+export { USSEnterprise2009 };
