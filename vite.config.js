2a2ca35abecb1a7509b8b50a9dd40b75487612d3
Rajkumar Baskar <rajkumarneville@gmail.com>
2023-12-31 05:12:03 -0500

    Reformat to include a new page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 // vite.config.js
-
+import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-export default {
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        home: './index.html', 
+        portfolio: './portfolio/index.html', 
+      },
+      output: {
+        chunkFileNames: 'chunks/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
   plugins: [
     viteStaticCopy({
       targets: [
@@ -12,5 +25,5 @@ export default {
         }
       ]
     })
-  ]
-}
+  ],
+});
