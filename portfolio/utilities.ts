cf2f8fe3e979d83988167dd04127fcf9fac03987
Rajkumar Baskar <rajkumarneville@gmail.com>
2024-01-03 16:19:37 -0500

    Return dummy data for json conversion

--- portfolio/utilities.ts
+++ portfolio/utilities.ts
@@ -28,6 +28,30 @@ function getJSONdoc(uri: string): Promise<JSON>{
 
 function jsonToGraph(data: JSON){
   console.log(data);
+  const nodes = new Map<string, string>();
+  const edges = new Map<string, string>();
+  // Add a dummy return for trial
+  return {
+    "nodes": [
+        {
+          "id": "id1",
+          "name": "name1",
+          "val": 1
+        },
+        {
+          "id": "id2",
+          "name": "name2",
+          "val": 10
+        },
+    ],
+    "links": [
+        {
+            "source": "id1",
+            "target": "id2"
+        },
+    ]
+  }
+
 }
 
 export { getJSONdoc, jsonToGraph };
