1898a9dca09da0f6aba743f72b85ba0f81948218
Rajkumar Baskar <rajkumarneville@gmail.com>
2024-01-02 22:45:40 -0500

    Add a data json and corresponding utilities to load it

--- home/utilities.ts
+++ home/utilities.ts
@@ -82,4 +82,34 @@ function attachMovements(model: iShip) {
   document.addEventListener('keydown', handleKeyPress);
 }
 
-export { placeModel, attachMovements, includeJoystick };
+function getJSONdoc(uri: string): Promise<JSON>{
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open('GET', uri, true);
+    xhr.onload = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        const json_response = JSON.parse(xhr.responseText);
+        resolve(json_response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
+}
+
+function jsonToGraph(data: JSON){
+  console.log(data);
+}
+
+export { placeModel, attachMovements, includeJoystick, getJSONdoc, jsonToGraph };
--- portfolio/main.ts
+++ portfolio/main.ts
@@ -1,6 +1,8 @@
 import './style.css';
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { getJSONdoc, jsonToGraph } from '../home/utilities';
+const portfolio_content_json = "../res/data/portfolio-content.json";
 
 // Create three main componenets: scene, camera and renderer
 const scene = new THREE.Scene();
@@ -8,7 +10,10 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 });
-const mixers: THREE.AnimationMixer[] = []
+const mixers: THREE.AnimationMixer[] = [];
+
+const portfolio_data = await getJSONdoc(portfolio_content_json);
+jsonToGraph(portfolio_data);
 
 // set renderer pixel ratio as device and fit window to full screen
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -19,17 +24,10 @@ const camOffset = new THREE.Vector3(0, 100, 200);
 camera.position.copy(camOffset);
 scene.add(camera);
 
-
 // Add orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.dampingFactor = 0.2;
 controls.rotateSpeed = 0.2;
-
-// Add a space backgroud as the texture to the scene
-// const spaceTexture = new THREE.TextureLoader().load(space_bg);
-// scene.background = spaceTexture;
-
-
 
 // Add a light to the scene
 const pointLight = new THREE.PointLight(0xFFFFFF);
@@ -41,23 +39,6 @@ scene.add(pointLight, ambientLight);
 const lightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(lightHelper);
 
-// Add some stars to the scene
-function addStar(){
-  const starGeometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const starMaterial = new THREE.MeshStandardMaterial({color:0xFFFFFF});
-  const star = new THREE.Mesh(starGeometry, starMaterial);
-
-  const [x,y,z] = Array(3).fill(0).map(() => THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x,y,z);
-  
-  scene.add(star);
-}
-
-// Populate stars on the scene
-Array(200).fill(0).forEach(addStar);
-
-// Render the scene
-const clock = new THREE.Clock();
 
 function animate(){
   requestAnimationFrame(animate);
