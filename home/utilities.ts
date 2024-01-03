a0fcd422e6254a9a3cd210d55e5a44a5b3fc5fb9
Rajkumar <54833237+rajkumar-b@users.noreply.github.com>
2024-01-03 00:04:13 -0500

    Split utilities (#50)

--- home/utilities.ts
+++ home/utilities.ts
@@ -82,34 +82,4 @@ function attachMovements(model: iShip) {
   document.addEventListener('keydown', handleKeyPress);
 }
 
-function getJSONdoc(uri: string): Promise<JSON>{
-  return new Promise(function (resolve, reject) {
-    const xhr = new XMLHttpRequest();
-    xhr.overrideMimeType("application/json");
-    xhr.open('GET', uri, true);
-    xhr.onload = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        const json_response = JSON.parse(xhr.responseText);
-        resolve(json_response);
-      } else {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
-      }
-    };
-    xhr.onerror = function () {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText
-      });
-    };
-    xhr.send();
-  });
-}
-
-function jsonToGraph(data: JSON){
-  console.log(data);
-}
-
-export { placeModel, attachMovements, includeJoystick, getJSONdoc, jsonToGraph };
+export { placeModel, attachMovements, includeJoystick };
---
+++ portfolio/utilities.ts
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+function getJSONdoc(uri: string): Promise<JSON>{
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open('GET', uri, true);
+    xhr.onload = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        const json_response = JSON.parse(xhr.responseText);
+        resolve(json_response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
+}
+
+function jsonToGraph(data: JSON){
+  console.log(data);
+}
+
+export { getJSONdoc, jsonToGraph };
